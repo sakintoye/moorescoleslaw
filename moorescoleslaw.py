1c1ad395f1e21fc1a2450ec2133c7de3ef6e581e
Jake Creps <creps7055@gmail.com>
2013-08-16 19:25:35 -0700

    Create moorescoleslaw.py
    First 7 years created.  User interaction is created.  Details are minute.  Would like to have the ability to create a switch to terahertz and pentahertz once the numbers become to great.

---
+++ moorescoleslaw.py
@@ -0,0 +1,40 @@
+#python
+#Moore's Coleslaw
+
+import sys        # to be able to exit the script
+
+# directory of years in relation to computing power
+year = {
+  "2010" : (5.31 * 2.0 ** 1.0) - 5.31,
+  "2011" : (5.31 * 2.0 ** 1.0) - 5.31 * 0.5,
+  "2012" : (5.31 * 2.0 ** 2.0) - 5.31 * 2.0,
+  "2013" : (5.31 * 2.0 ** 2.0) - 5.31 * (2.0 / 2),
+  "2014" : (5.31 * 2.0 ** 3.0) - 5.31 * 2.0 ** 2.0,
+  "2015" : (5.31 * 2.0 ** 3.0) - 5.31 * (2.0 ** 2.0 / 2),
+  "2016" : (5.31 * 2.0 ** 4.0) - 5.31 * 2.0 ** 3.0,
+  "2017" : (5.31 * 2.0 ** 4.0) - 5.31 * (2.0 ** 3.0 / 2),
+  }
+  
+speed = year[years]
+
+years = raw_input("Pick a year starting at 2010: ") 
+
+if years not in year:            			                                      
+    #check if we have year
+  i=0
+  while i <= 5:							                                          
+      #let's give user 5 tries
+    i+=1
+    print "We haven't gone that far back, try an earlier year or come back later."	      #print info
+    years = raw_input("Pick a different year: ") 			                        
+    #ask for new input
+    if years in year:
+      break
+    if i == 5:  						
+      print "Try again later!"  			                        #say bye
+      sys.exit()						                                         
+      #end of program
+else:					          # if star is in stars
+  pass					        # just continue
+  
+print "Moore's Law suggests consumer computing power in %r will be %r GHz." % (years, speed)
