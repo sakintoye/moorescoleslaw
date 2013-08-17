7a34f9db6b3dabcbeb049423780fce1c7deed62d
Jake Creps <creps7055@gmail.com>
2013-08-17 10:57:34 -0700

    Update moorescoleslaw.py

--- moorescoleslaw.py
+++ moorescoleslaw.py
@@ -4,14 +4,14 @@
 import sys        # to be able to exit the script
 
 def calculate_speed(year):
-    return 2 ** ((year - 2010) / 2) * 5.31
+    return 2 ** ((year - 2010) / 2) * 5.2
   
 year = raw_input("Pick a year: ") 
 
 try:
     speed = calculate_speed(float(year))
 except ValueError:
-    print "Sorry, this is not a legal number"
+    print "Sorry, only numbers after 2009."
     sys.exit(1)
   
 print "Moore's Law suggests consumer computing power in %s will be %.2f GHz." % (year, speed)
