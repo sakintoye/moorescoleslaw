d55cbaf7ba549670c4ce118a5d84c8781d85b3e4
Akintoye Sunkanmi <olasunkanmiakintoye@gmail.com>
2013-08-23 03:15:05 -0700

    Update moorescoleslaw.py
    Converted year from string to integer. Changed try/except to if/else

--- moorescoleslaw.py
+++ moorescoleslaw.py
@@ -6,12 +6,12 @@ import sys        # to be able to exit the script
 def calculate_speed(year):
     return 2 ** ((year - 2010) / 2.0) * 5.2
   
-year = raw_input("Pick a year: ") 
+year = raw_input("Pick a year: ")
 
-try:
+if int(year) > 2009:
     speed = calculate_speed(float(year))
-except ValueError:
+    print "Moore's Law suggests consumer computing power in %s will be %.2f GHz." % (year, speed)
+else:
     print "Sorry, only numbers after 2009."
-    sys.exit(1)
-  
-print "Moore's Law suggests consumer computing power in %s will be %.2f GHz." % (year, speed)
+
+sys.exit(1)
